87c8b2961a79c198fafa7bb15c519a1232157b17
Gustavo Guimarães <71523561+gguimaraes0@users.noreply.github.com>
2024-08-11 23:22:18 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,48 @@
+nome = 'stasi'  # string
+
+idade = 14  # inteiro(int)
+
+altura = 1.75
+
+lista_compras = ["presunto", "pao", "mortadela"]
+lista_numeros = [8, 4, 1]
+
+
+# item = lista_compras[0] + " e " + lista_compras[2]
+# print(item)
+
+# dados_pessoa = [{"nome": "Joao", "Idade": 40, "Altura": 1.80}, {"nome": "Maria", "Idade": 17, "Altura": 1.65}]
+
+# print(dados_pessoa[0])
+
+# tupla_coordenadas = (50,20,5)
+
+def calculaCompras(precomortadela, precopao, precoleite):
+    pagamento = precoleite + precopao + precomortadela
+    return pagamento
+
+def dobra(valor):
+    return valor*2
+
+precoleite = 5
+precopao = 1
+precomortadela = 6
+
+pagamento = 0
+pagamento = calculaCompras(precoleite, precopao, precomortadela)
+print(pagamento)
+pagamento = dobra(pagamento)
+
+print(pagamento)
+
+
+
+
+# if (pagamento > 10):
+#     print('Está liberado passar no cartão de crédito')
+# elif (pagamento == 50):
+#     print('Não vou vender')
+# elif (pagamento == 50):
+#     print('Não vou vender denovo')
+# else:
+#     print('Não está liberado passar no cartão de crédito')
